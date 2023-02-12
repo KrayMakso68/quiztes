5a898bf31e0937f014d90d13997b09cb62bab254
KrayMakso68 <makson6803@gmail.com>
2023-02-12 21:31:51 +0300

    finish create layout.html. Add model Subjects

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField('Название', max_length=20)
+    subject_number = models.IntegerField('Номер')
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
